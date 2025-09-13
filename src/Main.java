0712bf193f47e1e0e8a53fd463a3ede54898c420
Shrishti gupta <shrishtigupta2812@gmail.com>
2025-09-13 20:07:53 +0530

    add calculator changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        char operator=sc.next().charAt(0);
+        int num2=sc.nextInt();
+        switch (operator){
+            case '+':
+                System.out.println(num1+num2);
+                break;
+            case '-':
+                System.out.println(num1-num2);
+                 break;
+            case '*':
+                System.out.println(num1*num2);
+                break;
+            case '/':
+                System.out.println(num1/num2);
+                break;
+            case '%':
+                System.out.println(num1%num2);
+                break;
+            case '^':
+                System.out.println(Math.pow(num1,num2));
+                break;
+            default:
+                System.out.println("Invalid operator");
+        }
 
         }
     }
